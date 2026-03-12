b032f5251efe26e810947f54885ab279ec8200fa
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2026-03-12 17:39:32 -0500

    Mostrar torneos en la página principal

--- torneos/views.py
+++ torneos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Torneo
 # Create your views here.
 def lista_torneos(request):
-    return render(request, "torneos/lista.html")
+    torneos = Torneo.objects.all()
+    return render(request, "torneos/lista.html", {
+        "torneos": torneos
+    })
