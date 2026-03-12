c8014fc5b9f3456d8267b2e64f6627c59b395c81
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2026-03-12 17:27:32 -0500

    Modelo de Torneos y Admin configurado

--- torneos/models.py
+++ torneos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Torneo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+    cupos = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
+        
