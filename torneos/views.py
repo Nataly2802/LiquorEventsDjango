74a6cf2fc7b7a863d156dabb670427ac95d0bc04
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2026-03-13 16:19:13 -0500

    Evitar doble inscripcion y mostrar estado

--- torneos/views.py
+++ torneos/views.py
@@ -3,9 +3,19 @@ from django.contrib.auth.decorators import login_required
 from .models import Torneo, Inscripcion
 # Create your views here.
 def lista_torneos(request):
+    
     torneos = Torneo.objects.all()
+
+    inscritos = []
+
+    if request.user.is_authenticated:
+        inscritos = Inscripcion.objects.filter(
+            participante=request.user
+        ).values_list('torneo_id', flat=True)
+
     return render(request, "torneos/lista.html", {
-        "torneos": torneos
+        "torneos": torneos,
+        "inscritos": inscritos
     })
     
 @login_required
@@ -13,9 +23,17 @@ def inscribirse(request, torneo_id):
 
     torneo = get_object_or_404(Torneo, id=torneo_id)
 
-    Inscripcion.objects.create(
+    # verificar si ya está inscrito
+    existe = Inscripcion.objects.filter(
         participante=request.user,
         torneo=torneo
-    )
+    ).exists()
+
+    if not existe:
+
+        Inscripcion.objects.create(
+            participante=request.user,
+            torneo=torneo
+        )
 
     return redirect('lista_torneos')
