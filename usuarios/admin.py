071e6bf4f0865fa9da715abe79d7ae7cfae34d92
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2026-03-18 12:32:33 -0500

    Encriptcin de contraseñas

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
-# Register your models here.
-admin.site.register(Usuario)
+
+@admin.register(Usuario)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('rol',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('rol',)}),
+    )
